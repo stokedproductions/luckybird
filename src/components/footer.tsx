bf976b9e5b618d15d37bcb0bc1a6202d963733ba
stokedproductions <dean@stokedproductions.co.za>
2023-03-17 17:08:48 +0200

    last curves home

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <>
+      <div className='bg-black w-full h-[140px]'></div>
+      <div className='bg-[#6F7889] w-full h-[76px] flex justify-center items-center'>
+        <p className='text-[#D4D7DD] text-[13px] font-poppins'>
+        Dealr Automotive Limited    |    Registration number: 629444    |    15 Harcourt street, St. Kevin&amp;s, Dublin 2 D02 XY47, Dublin, Ireland
+        </p>
+      </div>
+    </>
+  )
 }
 
 export default Footer

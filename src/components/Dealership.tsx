f9b556ee5911aac6dc89c0f08ca862c556194f5c
stokedproductions <dean@stokedproductions.co.za>
2023-03-22 15:29:08 +0200

    import fixed

--- src/components/Dealership.tsx
+++ src/components/Dealership.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import IconCard from './IconCard'
+import IconCard from './IconCard';
 
 interface dealershipType {
   isForHome?: boolean;
